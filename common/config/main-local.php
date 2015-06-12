b488e4bb6afc844c489cc2013eed69de6a13f9c1
Hieu Tran Dinh <hieutd@smartosc.com>
2015-06-12 08:29:38 +0700

    update

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=gpis',
+            'dsn' => 'mysql:host=localhost;dbname=gpis88ce',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
